b8a60b19ffaf915d079c81ed583f298338b1a0dc
DakSmith11 <ddsbwh@umsystem.edu>
2023-12-07 00:50:40 -0600

    Fixed some small errors (I ran it I swear)

--- app/database_funcs.py
+++ app/database_funcs.py
@@ -708,10 +708,14 @@ def ModifyStudyHours(
 
     Args:
         student_id (int): Id of the member whos study hours are being modified
-        number_hours (int | None, optional): The amount of hours each day a member is to study for. Defaults to None.
-        can_video_game (bool | None, optional): Determines if a member can play video games (1) or not(0). Defaults to None.
-        social_probation (bool | None, optional): Determines if a member is on social probation(1) or not(0). Defaults to None.
-        conn (sqlite3.Connection | None, optional): The SQLite database connection. Defaults to None.
+        number_hours (int | None, optional):
+            The amount of hours each day a member is to study for. Defaults to None.
+        can_video_game (bool | None, optional):
+            Determines if a member can play video games (1) or not(0). Defaults to None.
+        social_probation (bool | None, optional):
+            Determines if a member is on social probation(1) or not(0). Defaults to None.
+        conn (sqlite3.Connection | None, optional):
+            The SQLite database connection. Defaults to None.
     """
     if not conn:
         conn = sqlite3.connect(DB_PATH)
@@ -913,5 +917,4 @@ def AssignFine(
 
 
 # ~TODO~
-# Assign Fine
 # Grade information (put in weekly schedule?)
